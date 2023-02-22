89298503c15b9398287634a3ab7cbbf9b644a015
Matthew Mould <mattdmould@gmail.com>
2023-02-22 15:57:15 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+name = 'pythonpackage'
+version = '0.0.1'
+
+with open('README.md' ,'r') as f:
+    long_description = f.read().strip()
+
+setup(
+    name=name,
+    version=version,
+    description=name,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/mdmould/{name}',
+    author='Matthew Mould',
+    author_email='mattdmould@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+    )
+    
+
