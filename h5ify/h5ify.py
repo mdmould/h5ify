71dab21ae944ea75bea8d766f5ac6c818fc7a1fa
Matthew Mould <mattdmould@gmail.com>
2025-03-12 17:12:09 -0400

    remove *args from save and add mode, reorganize functions

--- h5ify/h5ify.py
+++ h5ify/h5ify.py
@@ -1,50 +1,34 @@
 import h5py
 
+def load(file):
+    with open(file, 'r') as h:
+        return _recursive_load(h, {})
 
-def open(file, mode=None):
+def save(file, dic, mode = 'a', **kwargs):
+    with open(file, mode) as h:
+        _recursive_save(h, dic)
 
+def _open(file, mode = None):
     return h5py.File(file, mode) if type(file) is str else file
 
+def _recursive_load(h, d):
+    attrs = dict(h.attrs)
+    if len(attrs) > 0:
+        d['attrs'] = attrs
+    for key, val in h.items():
+        if isinstance(val, h5py.Group):
+            d[key] = {}
+            recursive(val, d[key])
+        elif isinstance(val, h5py.Dataset):
+            d[key] = val[()]
+    return d
 
-def load(file):
-
-    def recursive(h, d):
-
-        attrs = dict(h.attrs)
-        if len(attrs) > 0:
-            d['attrs'] = attrs
-     
-        for key, val in h.items():
-
-            if isinstance(val, h5py.Group):
-                d[key] = {}
-                recursive(val, d[key])
-
-            elif isinstance(val, h5py.Dataset):
-                d[key] = val[()]
-                
-        return d
-
-    with open(file, 'r') as h:
-        return recursive(h, {})
-
-
-def save(file, dic, *args, **kwargs):
-
-    def recursive(h, d):
-
-        for key, val in d.items():
-
-            if key == 'attrs':
-                h.attrs.update(d[key])
-
-            elif isinstance(val, dict):
-                h.create_group(key)
-                recursive(h[key], val)
-
-            else:
-                h.create_dataset(key, *args, data=val, **kwargs)
-
-    with open(file, 'a') as h:
-        recursive(h, dic)
-
+def _recursive_save(h, d):
+    for key, val in d.items():
+        if key == 'attrs':
+            h.attrs.update(d[key])
+        elif isinstance(val, dict):
+            h.create_group(key)
+            recursive(h[key], val)
+        else:
+            h.create_dataset(key, data = val, **kwargs)
